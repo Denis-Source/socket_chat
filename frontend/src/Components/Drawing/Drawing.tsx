d24138b54d06a6af2e3f4b25e976cf22f28845ea
Denis <denis.zol@code-care.pro@gmail.com>
2022-09-05 20:50:10 +0300

    fix drawing lines

--- frontend/src/Components/Drawing/Drawing.tsx
+++ frontend/src/Components/Drawing/Drawing.tsx
@@ -1,176 +1,178 @@
-import React, { useRef, useState } from "react";
-import { Layer, Line, Stage } from "react-konva";
+import React, {useRef, useState} from "react";
+import {Layer, Line, Stage} from "react-konva";
 import styles from "./Drawing.module.scss";
-import { CirclePicker } from "react-color";
+import {CirclePicker} from "react-color";
 import eraser from "../../Static/Images/eraser.svg";
 import pen from "../../Static/Images/pen.svg";
 import del from "../../Static/Images/delete.svg";
-import { prepareStatement, WSS_FEED_URL } from "../../api";
-import { TypeStatements } from "../../StatementsTypes/TypeStatements";
+import {prepareStatement, WSS_FEED_URL} from "../../api";
+import {TypeStatements} from "../../StatementsTypes/TypeStatements";
 import useWebSocket from "react-use-websocket";
-import { DrawingStatements } from "../../StatementsTypes/DrawingStatements";
-import { useDispatch, useSelector } from "react-redux";
+import {DrawingStatements} from "../../StatementsTypes/DrawingStatements";
+import {useDispatch, useSelector} from "react-redux";
 import {
-  addDrawingLine,
-  clearDrawing,
-  POINTS_LIMIT,
+    addDrawingLine,
+    clearDrawing,
+    resetLastLine, setLastLine, updateLastLine,
 } from "../../Reducers/Drawing";
-import { DrawingModel, LineModel } from "../../Models/Drawing.model";
+import {DrawingModel, LineModel} from "../../Models/Drawing.model";
 import Spinner from "../Spinner/Spinner";
-import { drawingColors } from "./DrawingColors";
+import {drawingColors} from "./DrawingColors";
 
 enum Tools {
-  pen = "pen",
-  eraser = "eraser",
+    pen = "pen",
+    eraser = "eraser",
 }
 
 const Drawing = () => {
-  const WIDTH = 640;
-  const HEIGHT = 480;
+    const WIDTH = 640;
+    const HEIGHT = 480;
 
-  const [tool, setTool] = useState(Tools.pen);
-  const [color, setColor] = useState("#000000cc");
-  const [lastLine, setLastLine] = useState<LineModel>();
-
-  const stageRef = useRef<any>(null);
-  const isDrawing = useRef(false);
-
-  // Configure websocket connection
-  const { sendJsonMessage } = useWebSocket(WSS_FEED_URL, {
-    share: true,
-  });
-
-  const drawing: DrawingModel = useSelector(
-    (state: any) => state.drawing.drawing
-  );
-  const dispatch = useDispatch();
-
-  const sendDelete = async () => {
-    const statement = prepareStatement({
-      type: TypeStatements.Call,
-      message: DrawingStatements.ResetDrawing,
+    // Configure websocket connection
+    const {sendJsonMessage} = useWebSocket(WSS_FEED_URL, {
+        share: true,
     });
-    await sendJsonMessage(statement);
-  };
 
-  // Function to update a drawn line
-  const sendUpdate = async () => {
-    if (lastLine) {
-      const statement = prepareStatement({
-        type: TypeStatements.Call,
-        message: DrawingStatements.ChangeDrawLine,
-        uuid: lastLine.uuid,
-        points: lastLine.points,
-        color: lastLine.color,
-        tool: lastLine.tool,
-      });
-      await sendJsonMessage(statement);
-    }
-  };
+    const drawing: DrawingModel = useSelector(
+        (state: any) => state.drawing.drawing
+    );
 
-  const handleMouseDown = async (e: any) => {
-    const pos = e.target.getStage().getPointerPosition();
-    isDrawing.current = true;
+    const lastLine: LineModel = useSelector(
+        (state: any) => state.drawing.lastLine
+    );
 
-    setLastLine({
-      tool: tool,
-      uuid: crypto.randomUUID(),
-      color: color,
-      points: [pos.x, pos.y],
-    });
-  };
+    const [tool, setTool] = useState(Tools.pen);
+    const [color, setColor] = useState("#000000cc");
 
-  const handleMouseMove = (e: any) => {
-    if (isDrawing.current && lastLine) {
-      const stage = e.target.getStage();
-      const pos = stage.getPointerPosition();
-      setLastLine({
-        tool: tool,
-        uuid: crypto.randomUUID(),
-        color: color,
-        points: [...lastLine.points, pos.x, pos.y].slice(-POINTS_LIMIT),
-      });
-    }
-  };
+    const stageRef = useRef<any>(null);
+    const isDrawing = useRef(false);
 
-  const handleMouseUp = async () => {
-    isDrawing.current = false;
-    lastLine && dispatch(addDrawingLine(lastLine));
-    await sendUpdate();
-  };
+    const dispatch = useDispatch();
 
-  const renderedLines = lastLine ? [...drawing.lines, lastLine] : drawing.lines;
+    const sendDelete = async () => {
+        const statement = prepareStatement({
+            type: TypeStatements.Call,
+            message: DrawingStatements.ResetDrawing,
+        });
+        await sendJsonMessage(statement);
+    };
 
-  return (
-    <>
-      {drawing.lines ? (
-        <div className={styles.layout}>
-          <Stage
-            width={WIDTH}
-            height={HEIGHT}
-            onMouseDown={handleMouseDown}
-            onMousemove={handleMouseMove}
-            onMouseup={handleMouseUp}
-            className={styles.stage}
-            ref={stageRef}
-          >
-            <Layer>
-              {renderedLines.map((line: LineModel, i: number) => (
-                <Line
-                  key={i}
-                  points={line.points}
-                  stroke={line.tool === "eraser" ? "#ffffff" : line.color}
-                  strokeWidth={line.tool === "eraser" ? 50 : 5}
-                  tension={0.5}
-                  lineCap="round"
-                  lineJoin="round"
-                  globalCompositeOperation={
-                    line.tool === "eraser" ? "destination-out" : "source-over"
-                  }
-                />
-              ))}
-            </Layer>
-          </Stage>
-          <div className={styles.toolsWrapper}>
-            <div className={styles.tools}>
-              <button
-                className={styles.button}
-                onClick={() => setTool(Tools.eraser)}
-              >
-                <img className={styles.buttonIcon} src={eraser} alt="eraser" />
-              </button>
-              <button
-                className={styles.button}
-                onClick={() => setTool(Tools.pen)}
-              >
-                <img className={styles.buttonIcon} src={pen} alt="pen" />
-              </button>
-              <button
-                className={styles.button}
-                onClick={async () => {
-                  dispatch(clearDrawing());
-                  await sendDelete();
-                }}
-              >
-                <img className={styles.buttonIcon} src={del} alt="delete" />
-              </button>
-            </div>
-            <CirclePicker
-              colors={drawingColors}
-              className={styles.picker}
-              width={"130px"}
-              onChange={(color) => {
-                setColor(`${color.hex}cc`);
-                setTool(Tools.pen);
-              }}
-            />
-          </div>
-        </div>
-      ) : (
-        <Spinner />
-      )}
-    </>
-  );
+    // Function to update a drawn line
+    const sendUpdate = async () => {
+        if (lastLine) {
+            const statement = prepareStatement({
+                type: TypeStatements.Call,
+                message: DrawingStatements.ChangeDrawLine,
+                uuid: lastLine.uuid,
+                points: lastLine.points,
+                color: lastLine.color,
+                tool: lastLine.tool,
+            });
+            await sendJsonMessage(statement);
+            dispatch(resetLastLine());
+        }
+    };
+
+    const handleMouseDown = async (event: any) => {
+        isDrawing.current = true;
+        const stage = event.target.getStage();
+        const pos = stage.getPointerPosition();
+
+        dispatch(setLastLine({
+            uuid: crypto.randomUUID(),
+            tool: tool,
+            color: color,
+            points: [pos.x, pos.y]
+        }))
+    };
+
+    const handleMouseMove = (event: any) => {
+        if (isDrawing.current && lastLine) {
+            const stage = event.target.getStage();
+            const pos = stage.getPointerPosition();
+            dispatch(updateLastLine([pos.x, pos.y]))
+        }
+    };
+
+    const handleMouseUp = async () => {
+        isDrawing.current = false;
+        lastLine && dispatch(addDrawingLine(lastLine));
+        await sendUpdate();
+        dispatch(resetLastLine());
+    };
+
+    const renderedLines = lastLine ? [...drawing.lines, lastLine] : drawing.lines;
+
+    return (
+        <>
+            {drawing.lines ? (
+                <div className={styles.layout}>
+                    <Stage
+                        width={WIDTH}
+                        height={HEIGHT}
+                        onMouseDown={handleMouseDown}
+                        onMousemove={handleMouseMove}
+                        onMouseup={handleMouseUp}
+                        className={styles.stage}
+                        ref={stageRef}
+                    >
+                        <Layer>
+                            {renderedLines.map((line: LineModel, i: number) => (
+                                <Line
+                                    key={i}
+                                    points={line.points}
+                                    stroke={line.tool === "eraser" ? "#ffffff" : line.color}
+                                    strokeWidth={line.tool === "eraser" ? 50 : 5}
+                                    tension={0.5}
+                                    lineCap="round"
+                                    lineJoin="round"
+                                    globalCompositeOperation={
+                                        line.tool === "eraser" ? "destination-out" : "source-over"
+                                    }
+                                />
+                            ))}
+                        </Layer>
+                    </Stage>
+                    <div className={styles.toolsWrapper}>
+                        <div className={styles.tools}>
+                            <button
+                                className={styles.button}
+                                onClick={() => setTool(Tools.eraser)}
+                            >
+                                <img className={styles.buttonIcon} src={eraser} alt="eraser"/>
+                            </button>
+                            <button
+                                className={styles.button}
+                                onClick={() => setTool(Tools.pen)}
+                            >
+                                <img className={styles.buttonIcon} src={pen} alt="pen"/>
+                            </button>
+                            <button
+                                className={styles.button}
+                                onClick={async () => {
+                                    dispatch(clearDrawing());
+                                    await sendDelete();
+                                }}
+                            >
+                                <img className={styles.buttonIcon} src={del} alt="delete"/>
+                            </button>
+                        </div>
+                        <CirclePicker
+                            colors={drawingColors}
+                            className={styles.picker}
+                            width={"130px"}
+                            onChange={(color) => {
+                                setColor(`${color.hex}cc`);
+                                setTool(Tools.pen);
+                            }}
+                        />
+                    </div>
+                </div>
+            ) : (
+                <Spinner/>
+            )}
+        </>
+    );
 };
 
 export default Drawing;
--- frontend/src/Reducers/Drawing.ts
+++ frontend/src/Reducers/Drawing.ts
@@ -1,41 +1,55 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { DrawingModel, LineModel } from "../Models/Drawing.model";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {DrawingModel, LineModel} from "../Models/Drawing.model";
 
 interface InitialState {
-  drawing: DrawingModel | null;
+    drawing: DrawingModel | null;
+    lastLine: LineModel | null;
 }
 
 const initialState: InitialState = {
-  drawing: null,
+    drawing: null,
+    lastLine: null
 };
 
 export const POINTS_LIMIT = 1000;
 export const LINES_LIMIT = 100;
 
 export const drawingSlice = createSlice({
-  name: "drawing",
-  initialState: initialState,
-  reducers: {
-    setDrawing: (state, action: PayloadAction<DrawingModel>) => {
-      state.drawing = action.payload;
-    },
-    clearDrawing: (state) => {
-      if (state.drawing) {
-        state.drawing.lines = [];
-      }
-    },
-    addDrawingLine: (state, action: PayloadAction<LineModel>) => {
-      if (state.drawing) {
-        state.drawing.lines = [...state.drawing.lines, action.payload].slice(
-          -LINES_LIMIT
-        );
-      }
-    },
-  },
+    name: "drawing",
+    initialState: initialState,
+    reducers: {
+        setDrawing: (state, action: PayloadAction<DrawingModel>) => {
+            state.drawing = action.payload;
+        },
+        clearDrawing: (state) => {
+            if (state.drawing) {
+                state.drawing.lines = [];
+            }
+        },
+        addDrawingLine: (state, action: PayloadAction<LineModel>) => {
+            if (state.drawing) {
+                if (!state.drawing.lines.find(line => line.uuid === action.payload.uuid)) {
+                    state.drawing.lines = [...state.drawing.lines, action.payload].slice(-POINTS_LIMIT);
+                }
+            }
+        },
+        setLastLine: (state, action: PayloadAction<LineModel>) => {
+            state.lastLine = action.payload;
+        },
+        updateLastLine: (state, action: PayloadAction<number[]>) => {
+            if (state.lastLine) {
+                state.lastLine.points = [...state.lastLine.points, ...action.payload].slice(-POINTS_LIMIT)
+            }
+        },
+        resetLastLine: (state) => {
+            state.lastLine = null;
+        }
+    }
 });
 
-export const { setDrawing, addDrawingLine, clearDrawing } =
-  drawingSlice.actions;
+export const {setDrawing, addDrawingLine, clearDrawing, updateLastLine, resetLastLine, setLastLine} =
+    drawingSlice.actions;
 export default drawingSlice.reducer;
 
-export class updateDrawing {}
+export class updateDrawing {
+}
