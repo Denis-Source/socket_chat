3c538dff544e4ecb44b1ba0b06c1d0de21909318
Denis <denis.zol@code-care.pro@gmail.com>
2022-11-23 06:36:37 +0200

    add missing files

---
+++ frontend/src/Layouts/InfoLayout/InfoLayout.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Strings } from "../../strings";
+import BaseLayout from "../BaseLayout/BaseLayout";
+import styles from "./InfoLayout.module.scss";
+import Ghost, { Animations } from "../../Components/Misc/Ghost/Ghost";
+import { Link } from "react-router-dom";
+import { RouterPaths } from "../../router";
+
+const InfoLayout = () => {
+    return (
+        <BaseLayout>
+            <div className={styles.wrapper}>
+                <div className={styles.header}>
+                    <h2>{Strings.InfoHeader}</h2>
+                </div>
+                <div className={styles.body}>
+                    <Ghost animation={Animations.Nod} />
+                    <p className={styles.desc}>{Strings.InfoP1}</p>
+                    <p className={styles.desc}>{Strings.InfoP2}</p>
+                    <p className={styles.desc}>
+                        <a
+                            className={styles.link}
+                            href="https://zoloto.cx.ua"
+                            target="_blank"
+                        >
+                            <u>{Strings.InfoLink}</u>
+                        </a>
+                    </p>
+                    <p className={styles.desc}>{Strings.InfoP3}</p>
+                    <p className={styles.desc}>
+                        {Strings.InfoP4}
+                        <a
+                            className={styles.link}
+                            href="https://github.com/Denis-Source/socket_chat"
+                            target="_blank"
+                        >
+                            <u>GitHub</u>
+                        </a>
+                    </p>
+                    <p className={styles.desc}>{Strings.InfoCopyRight}</p>
+                    <Link className={styles.desc} to={RouterPaths.HomePage}>
+                        <u>{Strings.GoBack}</u>
+                    </Link>
+                </div>
+            </div>
+        </BaseLayout>
+    );
+};
+
+export default InfoLayout;
